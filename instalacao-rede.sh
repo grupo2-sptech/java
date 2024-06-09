5f803ae37e4e2f891a5b396c2c1c6385d4480c65
Jonathan Carvalho <142412881+JonathanCarvalho39@users.noreply.github.com>
2024-06-09 19:42:57 -0300

    Atualizando link

--- instalacao-rede.sh
+++ instalacao-rede.sh
@@ -104,7 +104,7 @@ version: '3.8'
 services:
   mysql-server:
     container_name: mysql-server
-    image: joaogalhardo/mysql-server:5.7
+    image: jonathancarvalho039/mysql-server:5.7
     restart: always
     ports:
       - "3306:3306"
@@ -117,7 +117,7 @@ services:
 
   java-app-rede:
     container_name: java-app-rede
-    image: joaogalhardo/java-app-rede:17
+    image: jonathancarvalho039/java-app-rede:17
     restart: always
     depends_on:
       - mysql-server
